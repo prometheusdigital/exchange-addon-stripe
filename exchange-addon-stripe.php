b30a6bc8cc6fef0e9262c45fcd1b0a6f19e8e7bd
Lew Ayotte <lew@ithemes.com>
2014-03-04 14:19:23 -0600

    added mbstring nag, for users without mbstring enabled (breaks site because Stripe relies on it)

--- exchange-addon-stripe.php
+++ exchange-addon-stripe.php
@@ -26,20 +26,33 @@
  * @return void
 */
 function it_exchange_register_stripe_addon() {
-	$options = array(
-		'name'              => __( 'Stripe', 'LION' ),
-		'description'       => __( 'Process transactions via Stripe, a simple and elegant payment gateway.', 'LION' ),
-		'author'            => 'iThemes',
-		'author_url'        => 'http://ithemes.com/exchange/stripe/',
-		'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/stripe50px.png' ),
-		'wizard-icon'       => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/wizard-stripe.png' ),
-		'file'              => dirname( __FILE__ ) . '/init.php',
-		'category'          => 'transaction-methods',
-		'settings-callback' => 'it_exchange_stripe_addon_settings_callback',	
-	);
-	it_exchange_register_addon( 'stripe', $options );
+	if ( extension_loaded( 'mbstring' ) ) {
+		$options = array(
+			'name'              => __( 'Stripe', 'LION' ),
+			'description'       => __( 'Process transactions via Stripe, a simple and elegant payment gateway.', 'LION' ),
+			'author'            => 'iThemes',
+			'author_url'        => 'http://ithemes.com/exchange/stripe/',
+			'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/stripe50px.png' ),
+			'wizard-icon'       => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/wizard-stripe.png' ),
+			'file'              => dirname( __FILE__ ) . '/init.php',
+			'category'          => 'transaction-methods',
+			'settings-callback' => 'it_exchange_stripe_addon_settings_callback',	
+		);
+		it_exchange_register_addon( 'stripe', $options );
+	}
 }
 add_action( 'it_exchange_register_addons', 'it_exchange_register_stripe_addon' );
+
+function it_exchange_stripe_addon_show_mbstring_nag() {
+	if ( extension_loaded( 'mbstring' ) ) {
+		?>
+		<div id="it-exchange-add-on-mbstring-nag" class="it-exchange-nag">
+			<?php _e( 'You must have the mbstring PHP extension installed and activated on your web server to use the Stripe Add-on for iThemes Exchange. Please contact your web host provider to ensure this extension is enabled.', 'LION' ); ?>
+		</div>
+		<?php
+	}
+}
+add_action( 'admin_notices', 'it_exchange_stripe_addon_show_mbstring_nag' );
 
 /**
  * Loads the translation data for WordPress
