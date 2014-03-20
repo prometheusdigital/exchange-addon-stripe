af838948731c35d3a5bdcc030e0c8ae570923320
Lew Ayotte <lew@ithemes.com>
2014-03-20 11:51:03 -0500

    Tagging 1.1.24.1

--- exchange-addon-stripe.php
+++ exchange-addon-stripe.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Stripe Add-on
- * Version: 1.1.24
+ * Version: 1.1.24.1
  * Description: Adds the ability for users to checkout with Stripe.
  * Plugin URI: http://ithemes.com/exchange/stripe/
  * Author: iThemes
