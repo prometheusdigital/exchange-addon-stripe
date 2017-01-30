a2fed41a1ee5e36ac17fdaf144293137cf5c5a01
Timothy Jacobs <timothy@ironbounddesigns.com>
2017-01-29 21:24:04 -0500

    Use new method for iFrame integrations

--- lib/handlers/class.purchase.php
+++ lib/handlers/class.purchase.php
@@ -111,7 +111,7 @@ class IT_Exchange_Stripe_Purchase_Request_Handler extends ITE_IFrame_Purchase_Re
 
 		if ( $plan === false ) {
 			$cart->get_feedback()->add_error(
-				__ ('Purchase failed. Unable to create subscription.', 'it-l10n-ithemes-exchange' )
+				__( 'Purchase failed. Unable to create subscription.', 'it-l10n-ithemes-exchange' )
 			);
 
 			return null;
@@ -130,160 +130,37 @@ class IT_Exchange_Stripe_Purchase_Request_Handler extends ITE_IFrame_Purchase_Re
 
 		$config = $this->get_stripe_checkout_config( $request );
 
-		if ( ! $request->get_customer() instanceof IT_Exchange_Guest_Customer ) {
-			$tokens_endpoint = rest_url( "it_exchange/v1/customers/{$request->get_customer()->id}/tokens/" );
-			$tokens_endpoint = wp_nonce_url( $tokens_endpoint, 'wp_rest' );
-		} else {
-			$tokens_endpoint = '';
-		}
-
 		ob_start();
 		?>
-		<script type="text/javascript">
-			(function ( $ ) {
-				var tokensEndpoint = '<?php echo esc_js( $tokens_endpoint ); ?>',
-					newMethodLabel = '<?php echo esc_js( __( 'New Payment Method', 'LION' ) ); ?>',
-					completeLabel = '<?php echo esc_js( __( 'Complete Purchase', 'LION' ) ); ?>',
-					cancelLabel = '<?php echo esc_js( __( 'Cancel', 'LION' ) ); ?>',
-					$purchaseForm = $( '#stripe-purchase-form' );
+        <script type="text/javascript">
 
-				var stripeConfig = <?php echo wp_json_encode( $config ) ?>;
-				stripeConfig.token = function ( token ) {
+			jQuery( document ).ready( function ( $ ) {
 
-					it_exchange_stripe_processing_payment_popup();
+				itExchange.hooks.addAction( 'iFramePurchaseBegin.stripe', function ( deferred ) {
 
-					$purchaseForm.append( $( '<input type="hidden" name="to_tokenize">' ).val( token.id ) );
+					var fn = function () {
+						var stripeConfig = <?php echo wp_json_encode( $config ) ?>;
+						stripeConfig.token = function ( token ) {
+							deferred.resolve( { tokenize: token.id } );
+						};
+						stripeConfig.closed = function () {
+							deferred.resolve( { cancelled: true } );
+						};
 
-					$purchaseForm.submit();
-				};
-
-				$purchaseForm.submit( function ( e ) {
-
-					if ( $( "input[name='purchase_token'],input[name='to_tokenize']", $( this ) ).length ) {
-						return;
-					}
-
-					e.preventDefault();
-
-					$( this ).attr( 'disabled', true );
-
-					itExchange.stripeAddonCheckoutEmail = '<?php echo esc_js( $config['email'] ); ?>';
-					itExchange.hooks.doAction( 'itExchangeStripeAddon.makePayment' );
-
-					getTokens().then( function ( tokens ) {
-
-						if ( ! tokens.length ) {
-							StripeCheckout.open( stripeConfig );
-
-							return;
-						}
-
-						var html = buildTokenSelector( tokens );
-						html += '<input type="submit" value="' + completeLabel + '" id="it-exchange-stripe-complete-button">';
-						html += '<a href="#" id="it-exchange-stripe-cancel" style="width: 100%;	display: inline-block; text-align: center;">';
-						html += cancelLabel;
-						html += '</a>';
-
-						$( '.payment-methods-wrapper > form input, .it-exchange-purchase-button' ).hide();
-
-						$purchaseForm.append( '<div id="it-exchange-stripe-select-method">' + html + '</div>' );
-
-						if ( stripeConfig.plan ) {
-							$purchaseForm.append( $('<input type="hidden" name="stripe_subscription_id">').val(stripeConfig.plan) );
-						}
-
-					} ).fail( function ( err ) {
-						console.log( 'Stripe Tokens Error: ' + err );
+						itExchange.stripeAddonCheckoutEmail = stripeConfig.email;
+						itExchange.hooks.doAction( 'itExchangeStripeAddon.makePayment' );
 
 						StripeCheckout.open( stripeConfig );
-					} );
-				} );
+					};
 
-				$( document ).on( 'click', '#new-method-stripe', function ( e ) {
-					StripeCheckout.open( stripeConfig );
-				} );
-
-				$( document ).on( 'click', '#it-exchange-stripe-cancel', function ( e ) {
-					e.preventDefault();
-
-					$( "#it-exchange-stripe-select-method" ).remove();
-					$( '.payment-methods-wrapper > form, .it-exchange-purchase-button' ).show();
-				} );
-
-				// Prime the cache asynchronously.
-				getTokens();
-
-				/**
-				 * Get all Stripe Payment Tokens.
-				 *
-				 * This function will internally cache the HTTP request.
-				 *
-				 * @since 1.11.0
-				 *
-				 * @returns {*} Promise that resolves to a list of tokens.
-				 */
-				function getTokens() {
-
-					var promise = $.Deferred();
-
-					if ( ! tokensEndpoint.length ) {
-
-						return $.when( [] );
+					if ( !window.hasOwnProperty( 'StripeCheckout' ) ) {
+						jQuery.getScript( 'https://checkout.stripe.com/checkout.js', fn );
+					} else {
+						fn();
 					}
-
-					if ( this.hasOwnProperty( 'tokens' ) ) {
-						return $.when( this.tokens );
-					}
-
-					$.get( tokensEndpoint + '&gateway=stripe', ( function ( data, statusText, xhr ) {
-						if ( xhr.status !== 200 ) {
-							promise.reject( data );
-						} else {
-							this.tokens = data;
-							promise.resolve( data );
-						}
-					} ).bind( this ) );
-
-					return promise.promise();
-				}
-
-				/**
-				 * Build the tokens selector.
-				 *
-				 * @param {Object[]} tokens
-				 * @param {int} tokens[].id
-				 * @param {string} tokens[].label
-				 * @param {bool} tokens[].primary
-				 *
-				 * @returns {string}
-				 */
-				function buildTokenSelector( tokens ) {
-					var html = '<div class="it-exchange-credit-card-selector">';
-
-					for ( var i = 0, len = tokens.length; i < len; i ++ ) {
-
-						var token = tokens[ i ], c = '';
-
-						if ( token.primary ) {
-							c = ' checked="checked"';
-						}
-
-						html += '<label><input type="radio" name="purchase_token"' + c + ' value="' + token.id + '"> ';
-						html += token.label.rendered;
-						html += '</label><br>';
-					}
-
-					html += '<label><input type="radio" name="purchase_token" value="new_method" id="new-method-stripe"> ';
-					html += newMethodLabel;
-					html += '</label>';
-
-					html += '</div>';
-
-					return html;
-				}
-
-			})( jQuery );
-		</script>
+				} );
+			} );
+        </script>
 		<?php
 
 		return ob_get_clean();
