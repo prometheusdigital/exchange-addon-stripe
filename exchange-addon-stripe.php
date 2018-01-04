baf6584a7106f32f05f086c3f2ca53df9f2fa293
AJ Morris <aj@ajmorris.me>
2018-01-04 00:28:24 -0500

    Prepping the new updater to check the master license.

--- exchange-addon-stripe.php
+++ exchange-addon-stripe.php
@@ -111,29 +111,47 @@ function ithemes_exchange_stripe_deactivate() {
 }
 register_deactivation_hook( __FILE__, 'ithemes_exchange_stripe_deactivate' );
 
-	if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
-	 	require_once 'EDD_SL_Plugin_Updater.php';
-	 }
+// if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
+// 	require_once 'EDD_SL_Plugin_Updater.php';
+// }
 
 	function exchange_stripe_plugin_updater() {
+
+		$license_check = get_transient( 'exchangewp_license_check' );
+
+		if ($license_check->license == 'valid' ) {
+			$license_key = it_exchange_get_option( 'exchangewp_licenses' );
+			$license = $license_key['exchange_license'];
+
+			$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+					'version' 		=> '1.10.6', 				// current version number
+					'license' 		=> $license, 		// license key (used get_option above to retrieve from DB)
+					'item_name' 	=> 'stripe', 	  // name of this plugin
+					'author' 	  	=> 'ExchangeWP',    // author of this plugin
+					'url'       	=> home_url(),
+					'wp_override' => true,
+					'beta'		  	=> false
+				)
+			);
+		}
 
 		// retrieve our license key from the DB
 		// this is going to have to be pulled from a seralized array to get the actual key.
 		// $license_key = trim( get_option( 'exchange_stripe_license_key' ) );
-		$exchangewp_stripe_options = get_option( 'it-storage-exchange_addon_stripe' );
-		$license_key = $exchangewp_stripe_options['stripe_license'];
+		// $exchangewp_stripe_options = get_option( 'it-storage-exchange_addon_stripe' );
+		// $license_key = $exchangewp_stripe_options['stripe_license'];
 
 		// setup the updater
-		$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
-				'version' 		=> '1.10.6', 				// current version number
-				'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
-				'item_name' 	=> 'stripe', 	  // name of this plugin
-				'author' 	  	=> 'ExchangeWP',    // author of this plugin
-				'url'       	=> home_url(),
-				'wp_override' => true,
-				'beta'		  	=> false
-			)
-		);
+		// $edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+				// 'version' 		=> '1.10.6', 				// current version number
+				// 'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
+				// 'item_name' 	=> 'stripe', 	  // name of this plugin
+				// 'author' 	  	=> 'ExchangeWP',    // author of this plugin
+				// 'url'       	=> home_url(),
+				// 'wp_override' => true,
+				// 'beta'		  	=> false
+			// )
+		// );
 		// var_dump($edd_updater);
 		// die();
 
